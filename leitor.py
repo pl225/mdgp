a50352263694294f839c99de58afb269b36f399e
Matheus Abreu <matheusa596@gmail.com>
2019-10-19 11:05:28 -0300

    Comentando o código.

--- leitor.py
+++ leitor.py
@@ -14,22 +14,23 @@ class Instancia():
 	@staticmethod
 	def ler_arquivo(filename):
 		file = open(filename, "r")
-		info = file.readline().split()
-		n = int(info[0])
-		g = int(info[1])
+		info = file.readline().split() # divide a palavra inteira em uma lista de palavras conforme espaços em branco
+		n = int(info[0]) # quantidade de elementos
+		g = int(info[1]) # quantidade de grupos
 
 		limites = []
-		distancias = np.array([[0.] * n for _ in range(n)])
+		distancias = np.array([[0.] * n for _ in range(n)]) # iniciando todas as distancias entre pares de elementos como zero
 
-		if (info[2] == 'ds'):
+		# definição dos limites a e b para cada grupo
+		if (info[2] == 'ds'): # se os grupos têm tamanhos diferentes
 			for i in range(3, len(info), 2):
 				limites.append((int(info[i]), int(info[i + 1])))
-		else:
+		else: # os grupos têm tamanhos iguais
 			limites = [(int(info[3]), int(info[4])) for _ in range(g)]
 
 		limites = np.array(limites)
 
-		for linha in file:
+		for linha in file: # preenchendo as distancias entre cada par de elemento
 			linha = linha.split()
 			i, j, d = int(linha[0]), int(linha[1]), float(linha[2])
 			distancias[i][j] = distancias[j][i] = d
@@ -39,31 +40,31 @@ class Instancia():
 		return Instancia(n, g, limites, distancias)
 
 	def produzir_solucao(self):
-		nums = np.arange(self.n)
-		y = np.full(self.n, -1, dtype = int)
-		z = np.zeros(self.g, dtype = int)
+		nums = np.arange(self.n) # preenchimento de lista de 0 a n - 1 elementos
+		y = np.full(self.n, -1, dtype = int) # todos os elementos não estão associados a grupos no começo
+		z = np.zeros(self.g, dtype = int) # os grupos começam vazios
 
 		for i in range(self.g):
-			indices = np.random.choice(nums.size, self.limites[i][0], False)
-			y[nums[indices]] = i
-			z[i] = self.limites[i][0]
-			nums = np.delete(nums, indices)
+			indices = np.random.choice(nums.size, self.limites[i][0], False) # escolha aleatória de a_g elementos para o grupo g
+			y[nums[indices]] = i # todos os elementos selecionados são associados ao grupo i
+			z[i] = self.limites[i][0] # o número a de elementos para i foi selecionado
+			nums = np.delete(nums, indices) # exclusão dos elementos selecionados
 
 		limSup = self.limites[:, 1]
 
-		while nums.size > 0:
-			indice = randint(0, nums.size - 1)
-			grupos_nao_cheios = np.where(z < limSup)
-			grupo = np.random.choice(grupos_nao_cheios[0], 1)
-			y[nums[indice]] = grupo
-			z[grupo] += 1
-			nums = np.delete(nums, indice)
+		while nums.size > 0: # seleção dos elementos restantes
+			indice = randint(0, nums.size - 1) # seleção de um elemento
+			grupos_nao_cheios = np.where(z < limSup) # seleção dos grupos com menos que b elementos
+			grupo = np.random.choice(grupos_nao_cheios[0], 1) # escolha aleatória do grupo
+			y[nums[indice]] = grupo # associação do elemento ao grupo
+			z[grupo] += 1 # incrementando a quantidade de elementos do grupo
+			nums = np.delete(nums, indice) # exclusão do elemento associado
 
 		custo = 0
 
-		for i in range(self.g):
-			elems_grupo_i = np.where(y == i)[0]
-			custo += np.sum(self.distancias[elems_grupo_i][:, elems_grupo_i])
+		for i in range(self.g): # cálculo do custo da solução
+			elems_grupo_i = np.where(y == i)[0] # quais elementos são do grupo i
+			custo += np.sum(self.distancias[elems_grupo_i][:, elems_grupo_i]) # soma das distancias entre os elementos do grupo i
 
 		return Solucao(y, z, custo)
 
