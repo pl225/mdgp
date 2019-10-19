7cd7b78e7217d7790e21dff4c8e78a42e8e5388c
Matheus Abreu <matheusa596@gmail.com>
2019-10-19 10:45:34 -0300

    Implementação de construção de solução inicial aleatória.

--- leitor.py
+++ leitor.py
@@ -1,4 +1,7 @@
 import sys
+import numpy as np
+from random import randint
+from solucao_inicial import Solucao
 
 class Instancia():
 	
@@ -16,13 +19,15 @@ class Instancia():
 		g = int(info[1])
 
 		limites = []
-		distancias = [[0] * n for _ in range(n)]
+		distancias = np.array([[0.] * n for _ in range(n)])
 
 		if (info[2] == 'ds'):
 			for i in range(3, len(info), 2):
 				limites.append((int(info[i]), int(info[i + 1])))
 		else:
 			limites = [(int(info[3]), int(info[4])) for _ in range(g)]
+
+		limites = np.array(limites)
 
 		for linha in file:
 			linha = linha.split()
@@ -33,6 +38,35 @@ class Instancia():
 
 		return Instancia(n, g, limites, distancias)
 
+	def produzir_solucao(self):
+		nums = np.arange(self.n)
+		y = np.full(self.n, -1, dtype = int)
+		z = np.zeros(self.g, dtype = int)
+
+		for i in range(self.g):
+			indices = np.random.choice(nums.size, self.limites[i][0], False)
+			y[nums[indices]] = i
+			z[i] = self.limites[i][0]
+			nums = np.delete(nums, indices)
+
+		limSup = self.limites[:, 1]
+
+		while nums.size > 0:
+			indice = randint(0, nums.size - 1)
+			grupos_nao_cheios = np.where(z < limSup)
+			grupo = np.random.choice(grupos_nao_cheios[0], 1)
+			y[nums[indice]] = grupo
+			z[grupo] += 1
+			nums = np.delete(nums, indice)
+
+		custo = 0
+
+		for i in range(self.g):
+			elems_grupo_i = np.where(y == i)[0]
+			custo += np.sum(self.distancias[elems_grupo_i][:, elems_grupo_i])
+
+		return Solucao(y, z, custo)
+
 
 if __name__ == '__main__':
 	instancia = Instancia.ler_arquivo(sys.argv[1])
@@ -40,3 +74,5 @@ if __name__ == '__main__':
 	print(instancia.g)
 	print(instancia.limites)
 	print(instancia.distancias)
+	s = instancia.produzir_solucao()
+	print(s.y, s.z, s.f)
---
+++ solucao_inicial.py
@@ -0,0 +1,6 @@
+class Solucao():
+	def __init__(self, y, z, f):
+		self.y = y
+		self.z = z
+		self.f = f
+		
